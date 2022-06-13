20574dcf9c4eb92045e88c13284669fd23c67534
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-06-13 16:01:26 +0100

    end of module 2

--- ssg.py
+++ ssg.py
@@ -1,8 +1,9 @@
 import typer
 from ssg.site import Site
+import ssg.parsers
 
 def main(source, dest):
-    config = {'source':source, 'dest':dest}
+    config = {'source':source, 'dest':dest, 'parsers':[ssg.parsers.ResourceParser()]}
     print('config:',config)
     site = Site(**config).build()
 
--- ssg/site.py
+++ ssg/site.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers = None):
         print('In Site constructor')
         self.source = Path(source)
         self.dest = Path (dest)
+        self.parsers = parsers or []
         print(self.source,self.dest)
 
     def create_dir(self, path):
         path = Path(path)
         rel = path.relative_to(self.source)
         print('rel',rel)
-        directory = self.dest / rel
+        directory = self.dest / path.relative_to(self.source)
         print (directory)
 
         directory.mkdir(parents=True, exist_ok=True)
@@ -24,8 +25,22 @@ class Site:
             print(path)
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
             else:
-                print('not a dir')
+                print('This should never print... Uh-Oh!')
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print('No parser found for file type', path.suffix)
 
 
 #s = Site('/Users/sarahsloan/dev/LearningWorkspace/python-static-site-generator/content/', '/Users/sarahsloan/dev/LearningWorkspace/site_dest')
